a2a7f86253e9ddd79e1a715d2e80b5e82925360d
julio-iuli <carmo.iuli@gmail.com>
2016-03-04 15:26:43 +0000

    jogo de dados

--- araya/2016_02/jogo_dados/testeDados2.php
+++ araya/2016_02/jogo_dados/testeDados2.php
@@ -2,7 +2,27 @@
 
 include_once 'Dado.class.php';
 
-$d1 = new Dado(20);
-echo $d1->valor;
+$d1 = new Dado(6);
+$d2 = new Dado(6);
+$n = 1000; // número de lances dos dados
+
+// criando meu array de valores:
+for ($i=0; $i<13; $i++)
+{
+    $contagem[$i] = 0;
+}
+
+for ($i=0; $i<$n; $i++)
+{
+    $d1->jogar();
+    $d2->jogar();
+    $contagem[$d1->valor+$d2->valor] += 1;
+}
+
+for ($i=2; $i<13; $i++)
+{
+    echo "$i -> ".$contagem[$i]."<br>";
+}
+
 
 ?>
