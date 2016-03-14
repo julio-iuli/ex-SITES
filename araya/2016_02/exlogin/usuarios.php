bb3304df7594c188933bcf879cbbcb5c793c5cdb
julio-iuli <carmo.iuli@gmail.com>
2016-03-14 18:56:07 +0000

    exercicio feito

--- araya/2016_02/exlogin/usuarios.php
+++ araya/2016_02/exlogin/usuarios.php
@@ -1,5 +1,14 @@
 <?php
-	$usuarios = array(
-		
-	)
+
+
+$usuarios = array(
+	"Julio"=>"123456",
+	"Davi"=>"123456",
+	"Luciano"=>"123456",
+	"Diego"=>"123456",
+	"Ana"=>"123456",
+	"Pedro"=>"123456"
+);
+
+
 ?>
